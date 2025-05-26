8e87464ea86410a0c1b697f4eee342ea643b5644
yuvasekh <syuva893@gmail.com>
2025-05-26 07:43:19 +0530

    changes

--- index.js
+++ index.js
@@ -684,6 +684,16 @@ process.on('SIGTERM', async () => {
   }
   process.exit(0);
 });
+console.log(path.join(__dirname, 'build'));
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle all routing for React app
+app.get('', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 // Start server
 app.listen(PORT, () => {
