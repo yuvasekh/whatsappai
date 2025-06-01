17047b46e652230f0ce16d7b263c69ceb79437cf
yuvasekh <syuva893@gmail.com>
2025-06-01 09:02:22 +0530

    ADD: Screenshots after navigation and during message sending for better debugging

--- index.js
+++ index.js
@@ -1376,7 +1376,7 @@ process.on('SIGTERM', async () => {
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
+app.get('', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
